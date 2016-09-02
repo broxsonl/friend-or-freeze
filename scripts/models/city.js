a35dac78230440231492b510916c7f4a14d73c47
Maelle Vance <maellevance@gmail.com>
2016-09-02 16:17:59 -0700

    new database setup in webdb.js and insert records

--- scripts/models/city.js
+++ scripts/models/city.js
@@ -13,18 +13,38 @@
   City.createTable = function() {
     webDB.execute(
       'CREATE TABLE IF NOT EXISTS cities (' +
-        'id INTEGER PRIMARY KEY, ' +
-        'zip VARCHAR(255) NOT NULL, ' +
+        'zip INTEGER NOT NULL, ' +
         'state VARCHAR(255) NOT NULL, ' +
         'city VARCHAR (255), ' +
-        'lat FLOAT, ' +
-        'long FLOAT;',
+        'lat FLOAT NOT NULL, ' +
+        'lng FLOAT NOT NULL);',
       function() {
         console.log('Cities table successfully set up.');
       }
     );
   };
 
+  City.createTable();
+
+  //insert record in cities table for each City
+  City.prototype.insertRecord = function() {
+    webDB.execute(
+      [
+        {
+          'sql': 'INSERT INTO cities (zip, state, city, lat, lng) VALUES (?, ?, ?, ?, ?);',
+          'data': [this.zip, this.state, this.city, this.lat, this.lng],
+        }
+      ]
+    );
+  };
+
+
+  $.getJSON('/data/cities.json', function(rawData) {
+    rawData.forEach(function(item) {
+      var city = new City(item); // Instantiate a city based on item from JSON
+      city.insertRecord(); // Cache the article in DB
+    });
+  });
 
 
 
