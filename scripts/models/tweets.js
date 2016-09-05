fe582a16a4decb9c086c28609eae84945a9c7d49
Lee Broxson <broxsonl@gmail.com>
2016-09-04 20:56:00 -0700

    Added placeholder attr to index form input instead of old method.

--- scripts/models/tweets.js
+++ scripts/models/tweets.js
@@ -7,11 +7,13 @@
   tweetObj.lng = -122.342621; //TODO: get lng from webSQL
 
 
-  //this is our request to the server, we need to decide how many tweets we want, right now it's but we can get more easily. We will need to wrap it in a function to be triggered when we get the zipcode and add a call back for the next function we need to call :
+  //this is our request to the server, we need to decide how many tweets we want, right now it's 200 but we can get more easily. We will need to wrap it in a function to be triggered when we get the zipcode and add a call back for the next function we need to call :
   $.get('/search/tweets.json?q=&geocode=' + tweetObj.lat + ',' + tweetObj.lng + ',5mi&lang=en&count=200')
   .done(function(data, message, xhr) {
     tweetObj.all = data;
   });
+
+
 
   module.tweetObj = tweetObj;
 
