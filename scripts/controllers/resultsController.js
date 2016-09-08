98437b76e0919bff45e2a2e94a8e19bbae005314
Lee Broxson <broxsonl@gmail.com>
2016-09-08 09:44:16 -0700

    Added a fadeIn timer for rendered tweets.

--- scripts/controllers/resultsController.js
+++ scripts/controllers/resultsController.js
@@ -5,13 +5,13 @@
     $('.page-content').hide();
     console.log('Positive Vibes: ' + tweetObj.positives + ' , Negative Vibes: ' + tweetObj.negatives + ' , and Neutral Vibes: ' + tweetObj.neutrals);
     if (tweetObj.positives > tweetObj.negatives) {
-      $('#positive-results').fadeIn();
-      $('#positive-pic').fadeIn();
+      $('#positive-results').fadeIn(500);
+      $('#positive-pic').fadeIn(500);
     } else {
       $('#negative-results').fadeIn();
       $('#negative-pic').fadeIn();
     }
-    $('#example-tweets').fadeIn();
+    $('#example-tweets').fadeIn(3000);
     tweetsView.renderTweets();
     tweetObj.tweetText = [];
     tweetObj.positives = 0;
