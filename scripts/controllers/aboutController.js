bbab9f31c4863bec775da753ae12d4194d6fdb84
Maelle Vance <maellevance@gmail.com>
2016-09-08 17:34:20 -0700

    centered

--- scripts/controllers/aboutController.js
+++ scripts/controllers/aboutController.js
@@ -1,9 +1,10 @@
 (function(module) {
-  
+
   var aboutController = {};
 
   aboutController.reveal = function() {
     $('.page-content').hide();
+    $('.map-section').hide();
     $('#about').fadeIn();
     $('#about article').hide();
   };
